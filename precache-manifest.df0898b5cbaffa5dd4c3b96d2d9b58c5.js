2670fce7f5220f23318763c4a7df15a38c77f92e
Pengfei Xiao <pengfei123xiao@gmail.com>
2020-05-18 12:13:16 +1000

    Updates

---
+++ precache-manifest.df0898b5cbaffa5dd4c3b96d2d9b58c5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d998805442e88980267a3463862e473c",
+    "url": "/pizza-test/index.html"
+  },
+  {
+    "revision": "c2017dc01c8d9280b9c3",
+    "url": "/pizza-test/static/css/2.a0dd2848.chunk.css"
+  },
+  {
+    "revision": "003848fbcc0e75b50ee8",
+    "url": "/pizza-test/static/css/main.f4ef90b3.chunk.css"
+  },
+  {
+    "revision": "c2017dc01c8d9280b9c3",
+    "url": "/pizza-test/static/js/2.e6515a6f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pizza-test/static/js/2.e6515a6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "003848fbcc0e75b50ee8",
+    "url": "/pizza-test/static/js/main.ccbe68f5.chunk.js"
+  },
+  {
+    "revision": "c364efabdbc8fd7fde37",
+    "url": "/pizza-test/static/js/runtime-main.25a934fd.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/pizza-test/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/pizza-test/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/pizza-test/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/pizza-test/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/pizza-test/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
